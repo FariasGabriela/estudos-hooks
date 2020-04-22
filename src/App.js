a4996b2de0521e1d31ff268b866ab1bc2f5c3c68
Gabriela <gabriela.farias@kepha.com.br>
2020-04-22 16:53:40 -0300

    Estudos com styled-system

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Home from './view/Home';
+import styled from 'styled-components';
+import { color } from 'styled-system';
+
+const Item = styled.div`
+  ${color}
+`;
 
 function App() {
   return <Home />;
